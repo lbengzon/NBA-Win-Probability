00e6b8d41daae5d37621b8e9b020b6f74184bd0c
Leandro <lbengzon@users.noreply.github.com>
2017-06-07 16:30:32 +0800

    more progress

--- visualizations/predict.js
+++ visualizations/predict.js
@@ -16,29 +16,45 @@ $(document).ready(() => {
 		let minTimePassed = null
 		for (let i in plays) {
 			let play = plays[i]
-		    if(maxTimePassed < play.time_passed){
-		    	maxTimePassed = play.time_passed
-		    }
-		    if(minTimePassed === null || minTimePassed > play.time_passed){
-		    	minTimePassed = play.time_passed
-		    }
+			if(maxTimePassed < play.time_passed){
+				maxTimePassed = play.time_passed
+			}
+			if(minTimePassed === null || minTimePassed > play.time_passed){
+				minTimePassed = play.time_passed
+			}
 		}
-
-		var slider = document.getElementById('slider');
-		slider.style.width = width;
+		//get the slider element
+		let slider = $('#slider')[0];
+		//set the slider width
+		slider.style.width = width + "px";
 
 		slider.style.margin = '0 auto 30px';
+		let sliderTicks = getSliderTicks(minTimePassed, maxTimePassed);
 		noUiSlider.create(slider, {
 			start: [minTimePassed, maxTimePassed],
 			connect: true,
 			range: {
 				'min': minTimePassed,
 				'max': maxTimePassed
+			},
+			pips: {
+				mode: 'values',
+				values: sliderTicks,
+				density: 1
 			}
 		});
+		recountVal(3180);
+		$('.noUi-value.noUi-value-horizontal.noUi-value-large').each(function(){
+	        var val = $(this).html();
+	        val = recountVal(parseInt(val));
+	        $(this).html(val);
+	    });
+
+
+
 
 		//create the y scale using a domain from 0 to max distance
-	    let yScale = d3.scaleLinear().domain([1, 0]).range([0, height])
+		let yScale = d3.scaleLinear().domain([1, 0]).range([0, height])
 		
 
 		//Create the y axis with ticks of the percentage of home team winning the game
@@ -48,37 +64,38 @@ $(document).ready(() => {
 
 		// Create the SVG container and set the origin
 		let svg = d3.select("#chart").append("svg")
-		    .attr("width", width + margin.left + margin.right)
-		    .attr("height", height + margin.top + margin.bottom)
-		    .append("g")
-		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+		//Define the variables necessary to rescale and draw the x axis
 		let xScale, 
 		xAxis, 
 		xAxisVis = svg.append("g").attr("transform", "translate(0," + height + ")");
 
+		//draw the x axis based on the min time passed and max time passed found in the game
 		redrawXAxis(minTimePassed, maxTimePassed);
 
 
 		//Add the y axis
 		svg.append("g").call(yAxis);
 
-		//Add the paths to the svg
+		//create the g where the clipping rect and the path representing the winning probability of the home team will be appended.
 		let winProb = svg.append("g").attr("class", "winProb");
-
+		
+		//create the clipping rectangle so that when zooming, the line doesnt go passed the graph
 		var clip = winProb.append("defs").append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("id", "clip-rect")
-        .attr("x", "0")
-        .attr("y", "0")
-        .attr("width", width)
-        .attr("height", height);
+			.attr("id", "clip")
+			.append("svg:rect")
+			.attr("id", "clip-rect")
+			.attr("x", "0")
+			.attr("y", "0")
+			.attr("width", width)
+			.attr("height", height);
 
+		//draw the path representing the winning probability of the home team
 		drawWinPath();
-
-
-
 		//this is the where the tooltip circle will be added 
 		let focus = svg.append("g")
 			.style("display", "none");
@@ -93,12 +110,12 @@ $(document).ready(() => {
 		//Create the voronoi that will be used to find the nearest point
 		let voronoi = d3.voronoi()
 			.x(function(d) { return xScale(d.time_passed); })
-	  		.y(function(d) { return yScale(d.home_percentage); })
-	  		.extent([[0, 0], [width, height]]);
-	  	let voronoiData = voronoi(plays);
+			.y(function(d) { return yScale(d.home_percentage); })
+			.extent([[0, 0], [width, height]]);
+		let voronoiData = voronoi(plays);
 
-	    //append the rectangle which when interacted with, will affect the position and visibility of the tooltip
-	    svg.append("rect")
+		//append the rectangle which when interacted with, will affect the position and visibility of the tooltip
+		svg.append("rect")
 			.attr("width", width)
 			.attr("height", height)
 			.style("fill", "none")
@@ -110,6 +127,31 @@ $(document).ready(() => {
 		//Whenever the slider updates, redraw the graph.
 		slider.noUiSlider.on('update', redrawGraph);
 
+		function recountVal(timePassed){
+	        periodNumber = 0;
+	        leftOver = timePassed;
+	        while(leftOver > 0){
+	        	let secondsInPeriod = 0;
+	        	if(periodNumber < 4){
+		    		secondsInPeriod = 720;
+		    	} else{
+		    		secondsInPeriod = 300;
+		    	}
+	        	leftOver -= secondsInPeriod;
+	        	periodNumber += 1;
+	        }
+
+	        let periodString = getPeriodString(periodNumber);
+	        leftOver = -leftOver
+	        let min = Math.floor(leftOver/60);
+	        let seconds = leftOver % 60;
+	        if(seconds < 10){
+	        	seconds = "0" + seconds;
+	        }
+	        let playClock = min + ":" + seconds;
+	        return periodString + "<br>" + playClock;
+	    }
+
 		//Redraws the entire graph based on the new min and max time passed values
 		function redrawGraph(values){
 			redrawXAxis(values[0], values[1]);
@@ -118,6 +160,7 @@ $(document).ready(() => {
 			voronoiData = voronoi(plays);
 		}
 
+		//Draws the path of the win probability of the home team.
 		function drawWinPath(){
 			winProb.selectAll("path").remove();
 			winProb.selectAll("path").data([plays]).enter().append("path").attr("d", 
@@ -132,10 +175,10 @@ $(document).ready(() => {
 			periodTicks = getPeriodTicks(minTimePassed, maxTimePassed);
 			// Create the x axis with the ticks for the periods (e.g Q1, Q2)
 			xAxis = d3.axisBottom(xScale).tickValues(periodTicks).tickFormat(function(period, i){
-		    	return PERIOD_TICKS[period];
-		    }).tickSize(-height, 0);
-		    //Draw the axis
-		    xAxisVis.call(xAxis);
+				return PERIOD_TICKS[period];
+			}).tickSize(-height, 0);
+			//Draw the axis
+			xAxisVis.call(xAxis);
 		}
 
 		//Gets the ticks based on the the start and end of the graph
@@ -146,25 +189,38 @@ $(document).ready(() => {
 			ticks = Object.keys(PERIOD_TICKS);
 			for(let i in ticks){
 				let tick = ticks[i];
-				console.log(maxTimePassed);
-				console.log(tick);
 				if (tick >= minTimePassed && tick < maxTimePassed){
-					periodTicks.push(tick);
+					periodTicks.push(+tick);
 				}
 			}
 			return periodTicks;
 		}
 
+		function getSliderTicks(minTimePassed, maxTimePassed, tickSize){
+			tickSize = tickSize || 120;
+			let sliderTicks = [];
+			minTimePassed = Math.round(minTimePassed);
+			maxTimePassed = Math.round(maxTimePassed);
+			let tick = minTimePassed + tickSize - (minTimePassed % tickSize);
+			console.log(tick);
+			sliderTicks.push(tick);
+			while(tick < maxTimePassed){
+				tick += tickSize;
+				sliderTicks.push(tick);
+			}
+			return sliderTicks;
+		}
+
 		function mouseMove(){
 			const mouseX = d3.mouse(this)[0]
-	    	const mouseY = d3.mouse(this)[1]
-	    	//try to find a point that is close to the mouse cursor
-	    	const point = voronoiData.find(mouseX, mouseY, 50)
-	    	//if we have found a point within 50 px from the mouse
-	    	if(point !== null){
-	    		//Show the focus tool tip
-	    		focus.style("display", null);
-	    		//Move the circle to the point that was found
+			const mouseY = d3.mouse(this)[1]
+			//try to find a point that is close to the mouse cursor
+			const point = voronoiData.find(mouseX, mouseY, 50)
+			//if we have found a point within 50 px from the mouse
+			if(point !== null){
+				//Show the focus tool tip
+				focus.style("display", null);
+				//Move the circle to the point that was found
 				focus.select("circle.y")
 					.attr("transform",  
 						"translate(" + point[0] + "," +  
@@ -177,12 +233,12 @@ $(document).ready(() => {
 				let quartile = getQuartile(point);
 				//display the tooltip
 				nvtooltip.show([point[0] + margin.left, point[1] + margin.top], content, quartile)
-	    	} //if we haven't found a point
-	    	else{
-	    		//remove the tooltip
+			} //if we haven't found a point
+			else{
+				//remove the tooltip
 				nvtooltip.cleanup()
-	    		focus.style("display", "none");
-	    	}
+				focus.style("display", "none");
+			}
 		}
 
 		//returns the quartile that the mouse is in. Either 'q1', 'q2', 'q3', 'q3'
@@ -200,21 +256,43 @@ $(document).ready(() => {
 			return quartile;
 		}
 
+		function getPeriodString(periodNumber){
+			let period = "";
+			if(periodNumber < 5){
+				period = "Q" + periodNumber;
+			} else{
+				const ot = periodNumber - 4;
+				period = "OT" + ot;
+			}
+			return period;
+		}
+
+		function getPercentageChange(pointData){
+			let index = plays.indexOf(pointData);
+			if(index > 0){
+				let playBefore = plays[index - 1];
+				return convertToPercentage((+pointData.home_percentage) - (+playBefore.home_percentage))
+			}
+			else{
+				return convertToPercentage(pointData.home_percentage);
+			}
+		}
+
+		function convertToPercentage(decimal){
+			return Math.round(decimal * 10000)/100 + "%";
+		}
+
 		//given the point data, creates the content that will be inside the tool tip
 		function createToolTipContent(pointData){
 			//Change the winning percentage from decimal to a percentage rounded to the nearest hundredth
-			let winPercentage = Math.round(pointData.home_percentage * 10000)/100 + "%";
+			let winPercentage = convertToPercentage(pointData.home_percentage);
+			let percentageChange = getPercentageChange(pointData);
+
 
 			let score = pointData.score;
 			let playClock = pointData.play_clock
 			//Figure out the text for the quarter (eg. OT1, or Q4, we dont want any Q5)
-			let period = "";
-			if(pointData.period < 5){
-				period = "Q" + pointData.period;
-			} else{
-				const ot = pointData.period - 4;
-				period = "OT" + ot;
-			}
+			let period = getPeriodString(pointData.period);
 
 			//figure out the description based on which descriptions are null in the data
 			let description = "";
@@ -234,10 +312,10 @@ $(document).ready(() => {
 			//Create the actual content to be appended to the tooltip
 			let content = '<h3>' + score + '</h3>' + '<p>' +
 				'<span class="value">' + period + '-' + playClock + '</span><br>' +
-				'<span class="value">' + homeTeam + ' win percentage: ' + winPercentage + '</span><br>' +
+				'<span class="value">' + homeTeam + ' win percentage: ' + winPercentage + ' ('+percentageChange+')</span><br>' +
 				'<span class="value">' + description + '</span><br>' +
 				'</p>';
-            return content;
+			return content;
 		}
 
 		//Returns the path to draw on screen given the play predictions data
